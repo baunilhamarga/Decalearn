f2de7542dc4c13a903dd163871eca47538926ea9
baunilhamarga <129125831+baunilhamarga@users.noreply.github.com>
2023-09-12 00:57:53 -0300

    📝 change random state seed

--- Nasa predictive Maintenance (RUL)/nasa.py
+++ Nasa predictive Maintenance (RUL)/nasa.py
@@ -55,7 +55,7 @@ train[['unit_number','RUL']]
 from sklearn.model_selection import train_test_split
 drop_labels = index_names+setting_names
 X_train=train.drop(columns=drop_labels).copy()
-X_train, X_test, y_train, y_test=train_test_split(X_train,X_train['RUL'], test_size=0.3, random_state=42)
+X_train, X_test, y_train, y_test=train_test_split(X_train,X_train['RUL'], test_size=0.3, random_state=12227c)
 
  
 # from oil_static.py
