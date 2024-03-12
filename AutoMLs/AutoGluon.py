4ef74b4900f48c726028e73025b458f57940cef3
baunilha <heitor326@hotmail.com>
2024-03-12 09:34:16 -0300

    ✨ initial AutoGluon

---
+++ AutoMLs/AutoGluon.py
@@ -0,0 +1,48 @@
+from autogluon.tabular import TabularPredictor
+import numpy as np
+import random
+import copy
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn import preprocessing
+from sklearn.decomposition import PCA
+from pprint import pprint
+from sklearn.preprocessing import StandardScaler
+
+import autokeras as ak
+import tensorflow as tf
+
+
+if __name__ == '__main__':
+    random_state = 12227
+
+    tmp = np.load('/home/baunilha/Repositories/Decalearn/Datasets/GeologyTasks/FaciesClassification/FaciesClassificationYananGasField_coreset_1024.npz')
+    custom_project_name = 'AutoKeras/Facies_core'
+
+    X_train = tmp['X_train']
+    y_train = tmp['y_train']
+    X_test = tmp['X_test']
+    y_test = tmp['y_test']
+
+    n_classes = len(np.unique(y_train))
+
+    le = preprocessing.LabelBinarizer()
+    y_train = le.fit_transform(y_train)
+    y_test = le.fit_transform(y_test)
+
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
+
+
+    model = ak.StructuredDataClassifier(overwrite=True, max_trials=10, project_name=custom_project_name)
+
+    model.fit(tf.data.Dataset.from_tensor_slices((X_train.astype(str), y_train)), epochs=40)
+
+    y_pred = model.predict(tf.data.Dataset.from_tensor_slices((X_test).astype(str)))
+
+    acc_test = accuracy_score(np.argmax(y_test, axis=1), np.argmax(y_pred, axis=1))
+
+    print('Testing Accuracy [{:.4f}]'.format(acc_test))
+predictor = TabularPredictor(label="class").fit("train.csv")
+predictions = predictor.predict("test.csv")
