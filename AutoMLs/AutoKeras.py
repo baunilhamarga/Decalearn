e79703e8331d8c92f0a5585d1016298f35e1e04d
baunilha <heitor326@hotmail.com>
2024-03-12 23:56:54 -0300

    ⚡️ better autokeras

--- AutoMLs/AutoKeras.py
+++ AutoMLs/AutoKeras.py
@@ -15,8 +15,8 @@ import tensorflow as tf
 if __name__ == '__main__':
     random_state = 12227
 
-    tmp = np.load('/home/baunilha/Repositories/Decalearn/Datasets/GeologyTasks/FaciesClassification/FaciesClassificationYananGasField_coreset_1024.npz')
-    custom_project_name = 'AutoKeras/Facies_core'
+    tmp = np.load('../Datasets/GeologyTasks/FaciesClassification/FaciesClassificationYananGasField.npz')
+    custom_project_name = 'AutoKeras/Facies'
 
     X_train = tmp['X_train']
     y_train = tmp['y_train']
