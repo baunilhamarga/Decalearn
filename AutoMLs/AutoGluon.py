53f268a7ef7fc5583d02f2cedebff01255175e76
baunilha <heitor326@hotmail.com>
2024-03-15 15:30:08 -0300

    📝 organization + load predictor

--- AutoMLs/AutoGluon.py
+++ AutoMLs/AutoGluon.py
@@ -6,8 +6,7 @@ import pandas as pd
 if __name__ == '__main__':
     random_state = 12227 # AutoGluon doesn't let us change its seed, it uses seed 0 at every opportunity
 
-    tmp = np.load('../Datasets/GeologyTasks/FaciesClassification/FaciesClassificationYananGasField.npz')
-    custom_project_name = 'AutoKeras/Facies_core'
+    tmp = np.load('../Datasets/Multimodal Human Action/data/UTD-MHAD2_1s.npz')
 
     X_train = pd.DataFrame(tmp['X_train'])
     y_train = pd.DataFrame(tmp['y_train'], columns=['target'])
@@ -17,15 +16,20 @@ if __name__ == '__main__':
     train_data = pd.concat([X_train, y_train], axis=1)
     test_data = pd.concat([X_test, y_test], axis=1)
 
-    label = 'target' # Target to predict
+    label = 'target'
     time_limit = 133200  # 37h in seconds
-    metric = 'accuracy'  # Evaluation metric
-    path='AutoGluon/Facies_37h' # Save directory
+    metric = 'accuracy'  # evaluation metric
+    path = 'AutoGluon/MAHD_37h'
     # Recommended settings for maximizing predictive performance
     predictor = TabularPredictor(label, eval_metric=metric, path=path, log_to_file=True).fit(train_data, time_limit=time_limit, presets='best_quality')
+
+    # Load the predictor by specifying the path it is saved to on disk.
+    #predictor = TabularPredictor.load("Autogluon/Facies_37h")
 
     y_pred = predictor.predict(test_data)
 
     print(predictor.evaluate(test_data))
 
-    print(predictor.leaderboard(test_data))
+    leaderboard = predictor.leaderboard(test_data)
+    print(leaderboard)
+    leaderboard.to_csv('leaderboard_MAHD_37h.csv', index=False)
