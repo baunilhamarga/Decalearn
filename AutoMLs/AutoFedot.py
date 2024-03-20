6ff10f6e36f189d8ce206f874397cac50c1138a1
baunilha <heitor326@hotmail.com>
2024-03-20 01:28:17 -0300

    🚀 run autofedot

--- AutoMLs/AutoFedot.py
+++ AutoMLs/AutoFedot.py
@@ -1,32 +1,38 @@
 import numpy as np
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-from sklearn import preprocessing
-from sklearn.decomposition import PCA
-from pprint import pprint
-import autosklearn.classification
 from fedot.api.main import Fedot
-
-
+import pandas as pd
+from fedot.core.pipelines.pipeline import Pipeline
 
 
 if __name__ == '__main__':
     random_state = 12227
 
-    tmp = np.load('Nasa predictive Maintenance (RUL)/data_nasa.npz')
+    file_paths = [
+        '../Datasets/Multimodal Human Action/data/UTD-MHAD2_1s.npz',
+        '../Datasets/GeologyTasks/FaciesClassification/FaciesClassificationYananGasField.npz',
+        '../Datasets/Lucas/osha_train_test.npz',
+        '../Datasets/AI_text/AI_text.npz',
+        '../Datasets/CSI/dataset_csi.npz',
+        '../Datasets/AI_text/AI_text.npz',
+    ]
 
-    X_train = tmp['X_train']
-    y_train = tmp['y_train']
-    X_test = tmp['X_test']
-    y_test = tmp['y_test']
+    tmp = np.load(file_paths[0])
 
-    n_classes = len(np.unique(y_train))
+    X_train = pd.DataFrame(tmp['X_train'])
+    y_train = pd.DataFrame(tmp['y_train'], columns=['target'])
+    X_test = pd.DataFrame(tmp['X_test'])
+    y_test = pd.DataFrame(tmp['y_test'], columns=['target'])
 
-    cls = Fedot(problem='classification', timeout=5, preset='best_quality', n_jobs=-1, seed=random_state)
-    cls.fit(features=X_train, target=y_train)
-    prediction = cls.predict(features=X_test)
+    timeout = 2220 # 37h in minutes
+    model = Fedot(problem='classification', timeout=timeout, preset='best_quality', n_jobs=-1, seed=random_state)
+    best_pipeline = model.fit(features=X_train, target=y_train)
+    y_pred = model.predict(features=X_test)
 
-    y_pred = cls.predict(X_test)
+    print(model.get_metrics(target=y_test, metric_names=['acc']))
     acc = accuracy_score(y_test, y_pred)
-
     print("Accuracy score [{}]".format(acc))
+
+    path_to_save = 'AutoFedot/MAHD_37h'
+    best_pipeline.save(path=path_to_save, create_subdir=True, is_datetime_in_path=True)
+    #pipeline = Pipeline().load(path) # To load model
